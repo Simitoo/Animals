82b55bad7bdb923e21feda59c05e3be4b2d3e743
Simona Bueva <simona0821@gmail.com>
2023-02-20 09:19:33 +0200

    FirstStep

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -3,35 +3,106 @@
 // Only predators can move?
 
 //2nd moving predators and everytime they move, matrix should be changed
-// empty spaces with "_", predators "X", herbivores "O"
-//when moving you should check if X is on O or is on _
-//  -> if is _ then the symbol should be X
-//  -> else if O then have a 30% chance to be eaten => O changed to X or E(eaten) or _ because we should do something when is E
-//  -> what happens when X is on X? =>
+//predators "X", herbivores "O"
+//when moving you should check if X is on O 
+//  -> if O then have a 30% chance to be eaten => E(eaten) we should do something
+//  -> what happens when X is on X? 
 
 //moving stop when there isn't left any O
 
-char[,] area = new char[10,10];
 
-for(int row = 0; row < area.GetLength(0); row++)
+
+
+
+using Animals;
+
+
+
+
+Console.BufferHeight = Console.WindowHeight;
+
+
+
+Positions[] positions = new Positions[]
 {
-    for(int col = 0; col < area.GetLength(1); col++)
-    {
-        area[row, col] = '_';
-    }
+    new Positions(-1, 0), // u
+    new Positions(1, 0), // d
+    new Positions(0, -1), // l 
+    new Positions(0, 1), // r   
+};
+
+int currPosition = 0;
+
+
+Random random = new Random();
+
+List<Positions> predators = new List<Positions>();
+List<Positions> herbivores = new List<Positions>();
+
+for (int i = 0; i <= 10; i++)
+{
+    predators.Add(new Positions(random.Next(0, Console.WindowWidth), random.Next(0, Console.WindowHeight)));
+    herbivores.Add(new Positions(random.Next(0, Console.WindowWidth), random.Next(0, Console.WindowHeight)));
 }
 
-PrintMatrix(area);
-
-static void PrintMatrix(char[,] area)
+foreach (Positions pos in predators)
 {
-    for(int row = 0; row < area.GetLength(0); row++)
+    Console.SetCursorPosition(pos.Row, pos.Col);
+    Console.Write("X");
+}
+
+foreach (Positions pos in herbivores)
+{
+    Console.SetCursorPosition(pos.Row, pos.Col);
+    Console.Write("O");
+}
+
+while (true)
+{
+    Positions nextMove;
+
+    for (int i = 0; i < predators.Count; i++)
     {
-        for(int col = 0; col < area.GetLength(1); col++)
+        int directions = random.Next(0, 4);
+
+        switch (directions)
         {
-            Console.Write(area[row, col]);
+            case 0:
+                currPosition = 0;
+                break;
+            case 1:
+                currPosition = 1;
+                break;
+            case 2:
+                currPosition = 2;
+                break;
+            case 3:
+                currPosition = 3;
+                break;
         }
 
-        Console.WriteLine();
+        nextMove = positions[currPosition];
     }
+
+    //foreach (Positions pos in predators)
+    //{
+    //    Positions newPredators = new Positions(pos.Row + nextMove.Row, pos.Col + nextMove.Row);
+    //    predators.Remove(pos);
+    //    predators.Add(newPredators);
+    //}
+
+    //Console.Clear();
+
+    //foreach (Positions pos in predators)
+    //{
+    //    Console.SetCursorPosition(pos.Row, pos.Col);
+    //    Console.Write("X");
+    //}
+
+    //Thread.Sleep(100);
+
+
+
+
+
 }
